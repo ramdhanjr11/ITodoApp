82ad412578f15aaa8cd6559f08369e2a149a7d1f
Muramsyah <ramdhanjr690@gmail.com>
2025-02-19 12:07:21 +0700

    core: initial project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'splash_page.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: false,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
