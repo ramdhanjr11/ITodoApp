c0fcf90803f19c208fe134d780b1bbbed9fbfb8f
Muramsyah <ramdhanjr690@gmail.com>
2025-02-20 08:09:16 +0700

    update: add pretty dio logger

--- lib/config/app_di.dart
+++ lib/config/app_di.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:todo/data/repositories/todo_repository.dart';
 import 'package:todo/data/services/api_service.dart';
 import 'package:todo/domain/use_cases/todo_use_case.dart';
@@ -19,5 +21,8 @@ Dio configureDio() {
     connectTimeout: Duration(seconds: 5),
     receiveTimeout: Duration(seconds: 3),
   );
-  return Dio(options);
+  final dio = Dio(options);
+  dio.interceptors.add(PrettyDioLogger(enabled: kDebugMode));
+
+  return dio;
 }
